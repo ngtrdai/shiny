727d86c01072ba36b346b749bb5bf63d891194b7
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2024-06-25 17:04:11 +0700

    CI-CD: Test PR Sonar

--- copilot-api/settings.py
+++ copilot-api/settings.py
@@ -1,1 +1,11 @@
-c
+import os
+
+from pydantic.v1 import BaseSettings
+
+
+class Settings(BaseSettings):
+    KEYCLOAK_URL: str = os.getenv("KEYCLOAK_URL")
+    KEYCLOAK_REALM: str = os.getenv("KEYCLOAK_REALM")
+    KEYCLOAK_CLIENT_ID: str = os.getenv("KEYCLOAK_CLIENT_ID")
+    KEYCLOAK_CLIENT_SECRET: str = os.getenv("KEYCLOAK_CLIENT_SECRET")
+    
