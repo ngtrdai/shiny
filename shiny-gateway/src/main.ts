0b2c4c3426786b4e5a2eceefcb55d4aaec5ae9e2
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2024-06-26 18:00:08 +0700

    Init 1

--- shiny-gateway/src/main.ts
+++ shiny-gateway/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Shiny API Gateway')
+    .setDescription('The Shiny API Gateway')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then(() => console.log('API Gateway is running'));
