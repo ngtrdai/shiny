a7edaa31680ef8e5c3775a2e53434d1ed9653af8
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2024-06-26 18:00:08 +0700

    Init 1

--- shiny-gateway/src/app.module.ts
+++ shiny-gateway/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/users/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
